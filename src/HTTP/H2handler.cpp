3cb32fa24cdb1d739ff9f1fc556707dc57a09af0
Frederick Woodruff <frederickwoodruff@192.168.1.199>
2024-09-22 13:33:09 +0100

    tidy

--- src/HTTP/H2handler.cpp
+++ src/HTTP/H2handler.cpp
@@ -2,9 +2,9 @@
 
 #include "../Runtime/task.hpp"
 #include "H2handler.hpp"
+#include "HTTP2.hpp"
 
 namespace fbw {
-
 
 task<void> handle_stream(std::weak_ptr<HTTP2> connection, uint32_t stream_id) {
     // await on connection, woken by data available

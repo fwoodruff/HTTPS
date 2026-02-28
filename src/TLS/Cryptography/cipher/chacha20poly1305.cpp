e85bd1a6e76b1bd30b06cc90afb1f38b0fadf0cd
Frederick Woodruff <frederickwoodruff@192.168.1.111>
2026-02-28 19:32:05 +0000

    rotl

--- src/TLS/Cryptography/cipher/chacha20poly1305.cpp
+++ src/TLS/Cryptography/cipher/chacha20poly1305.cpp
@@ -23,15 +23,12 @@ namespace fbw::cha {
 
 
 
-constexpr uint32_t ROT32(uint32_t x, int shift) {
-    return (x << shift) | (x >> (32 - shift));
-}
 
 void chacha_quarter_round(uint32_t& a, uint32_t& b, uint32_t& c, uint32_t& d) {
-    a += b; d ^= a; d = ROT32(d, 16);
-    c += d; b ^= c; b = ROT32(b, 12);
-    a += b; d ^= a; d = ROT32(d, 8);
-    c += d; b ^= c; b = ROT32(b, 7);
+    a += b; d ^= a; d = std::rotl(d, 16);
+    c += d; b ^= c; b = std::rotl(b, 12);
+    a += b; d ^= a; d = std::rotl(d, 8);
+    c += d; b ^= c; b = std::rotl(b, 7);
 }
 
 
